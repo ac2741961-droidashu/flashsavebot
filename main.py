95fbe52c9cabaa1bf0342c1338bd9a4757050423
ac2741961-droidashu <ac2741961@gmail.com>
2026-01-16 04:38:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import osfrom telegram import Update
+import os
+from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
 TOKEN = os.getenv("HTTP API:
 8573872030:AAFTVQeD6sC7s0aF_GZ6eUB-5pRxKIvkv6Y")
 
@@ -7,7 +9,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("FlashSave Bot is online ⚡")
 
 async def help_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Send any message. Features coming soon.")
+    await update.message.reply_text("Send any message. More features coming soon.")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Received: " + update.message.text)

1d4f77f032a76e29617b8b324da9acb670be76a8
ac2741961-droidashu <ac2741961@gmail.com>
2026-01-16 04:07:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from telegram import Update
+import osfrom telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("FlashSave Bot is online ⚡")
